c4085b155ddc47a07c4a618866a488e4482953f7
Arts <1184664659@qq.com>
2023-03-19 10:59:08 +0800

    🦄 refactor(admin.py): 优化后台管理显示

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from . import models
 
-# 注册后台菜单
-admin.site.register(models.User)
+
+
+@admin.register(models.User)
+class User(admin.ModelAdmin):
+    list_display = ('username', 'email')
+    search_fields = ('username', 'email')
+    # list_filter = ('is_staff', 'is_active', 'is_superuser')
+    # ordering = ('-date_joined',
